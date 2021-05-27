97567eacdb39118041be936321d8c8c698c4988b
luiscadiz <luiscadiz1504@gmail.com>
2021-05-27 13:50:37 -0300

    Creando cards con display flex

---
+++ CreandoCards/js/main.js
@@ -0,0 +1,9 @@
+const button1 = document.querySelector('button');
+const evaluar1 = document.getElementById('aryResult');
+const evaluar2 = document.getElementById('sansaResult');
+const evaluar3 = document.getElementById('jonResult');
+
+button1.addEventListener("click",() => {
+    evaluar1.style.opacity = 1;
+})
+
